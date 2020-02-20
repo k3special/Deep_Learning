ec37ff0f4aad93e2321ea77a22b063b9108926ba
MIRAGE <61101642+k3special@users.noreply.github.com>
2020-02-20 22:49:16 +0900

    Update cifar.py

--- Image_Classification/cifar.py
+++ Image_Classification/cifar.py
@@ -1,1 +1,25 @@
+#!/usr/bin/env python
 
+import sys
+import cPickle
+
+def load_batch(fpath, label_key='labels'):
+    with open(fpath,'rb') as f:
+        d = cPickle.load(f)
+
+    data = d['data']
+    labels = d[label_key]
+
+    data = data.reshape(data.shape[0], 3, 32, 32)
+
+    return data, labels
+
+
+if __name__=='__main__':
+
+    path = 'cifar-10-batches-py/'
+    file = 'data_batch_1'
+
+    data, labels = load_batch(path+file)
+
+    print(data, labels)
